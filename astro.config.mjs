d9a1ee4d760001e54ecf9e2a64b567a4af15269c
minhnh <minhnh.da@gmail.com>
2026-02-21 12:31:17 +0900

    add sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
 
 export default defineConfig({
   site: 'https://tsukie.com',
@@ -10,5 +12,11 @@ export default defineConfig({
     tailwind(),
     react(),
     sitemap(),
+    sentry({
+      org: import.meta.env.SENTRY_ORG,
+      project: import.meta.env.SENTRY_PROJECT,
+      authToken: import.meta.env.SENTRY_AUTH_TOKEN,
+    }),
+    spotlightjs(),
   ],
 });
