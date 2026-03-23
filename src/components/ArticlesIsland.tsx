159b9be2bbdcbcb3f4488cfedb7d97bf49b2d53b
minhnh <minhnh.da@gmail.com>
2026-03-23 13:00:32 +0900

    add arrow to article item

--- src/components/ArticlesIsland.tsx
+++ src/components/ArticlesIsland.tsx
@@ -61,6 +61,11 @@ function ArticleCard({
           {post.summary}
         </div>
       </div>
+      <div className="absolute bottom-3 right-3 z-10 text-sky-500">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
+          <path fillRule="evenodd" d="M3 10a.75.75 0 0 1 .75-.75h10.638l-3.96-4.158a.75.75 0 1 1 1.08-1.04l5.25 5.5a.75.75 0 0 1 0 1.04l-5.25 5.5a.75.75 0 1 1-1.08-1.04l3.96-4.158H3.75A.75.75 0 0 1 3 10Z" clipRule="evenodd" />
+        </svg>
+      </div>
     </article>
   );
 }
